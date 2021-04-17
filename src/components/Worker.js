d1101ecf48e2da6683eee1ab07a8c6beb17761a8
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-17 12:59:20 -0400

    updating reducer function

--- src/components/Worker.js
+++ src/components/Worker.js
@@ -1,16 +1,21 @@
 import React, {useEffect, useState, useReducer} from 'react';
-import useWorker from "../hooks/useWorker";
 const ACTIONS = {
     Upgrade: "UpgradeWorker",
     Buy: "BuyWorker",
     Mining: "MiningWorker",
 }
-function reducer(state, action) {
+function reducer(state, action, payload) {
     switch(action) {
         case ACTIONS.Upgrade:
-            // Upgrade function
+            return {
+                ...state,
+                productionRate: this.productionRate + 1, 
+            }
         case ACTIONS.Buy:
-            // Buy function
+            return {
+                ...state,
+                owned: this.owned + payload.amount, 
+            }
         case ACTIONS.Mining:
             // Mining function
         default: return state
@@ -19,53 +24,23 @@ function reducer(state, action) {
 
 export default function Worker({worker, money, setMoney, multiplier}) {
     // set default state to worker and pass reducer function
-    const [state, dispatch] = useReducer(worker, reducer);
+    const [state, dispatch] = useReducer(reducer, worker);
     const miningSpeed = 10000;
     const [visible, setVisible] = useState(true);
-    const [ 
-        owned, setOwned, 
-        cost, setCost, 
-        productionRate, setProductionRate, 
-        productionRateUpgradeCost, setProductionRateUpgradeCost] = useWorker(worker)
-    const buyWorker = () => {
-        // check if there is enough money
-        const totalCost = cost * multiplier;
-        if (money < totalCost) return;
-        // if there is subtract from funds
-        setMoney(prevMoney => prevMoney - totalCost);
-        // adjust the stats accoringly
-        setOwned(prevOwned => prevOwned + multiplier);
-        setCost(prevCost => prevCost + multiplier);
-    };
-    // upgrade the workers production rate
-    const upgradeWorker = () => {
-        if (productionRateUpgradeCost > money) return;
-        setMoney(prevMoney => prevMoney - productionRateUpgradeCost);
-        setProductionRate(prevProductionRate => prevProductionRate + 1);
-        setProductionRateUpgradeCost(prevProductionRateUpgradeCost => prevProductionRateUpgradeCost * 2);
-    };
-    // the functoin for whenever our worker mines
-    const miningWorker = () => {
-        setMoney(prevMoney => prevMoney + owned * productionRate);
-    };
-    // restart the mining function whenever we buy another miner
-    useEffect(() => {
-        setInterval(() => miningWorker(worker), miningSpeed)
-    }, [owned])
     const reveal = () => {
         setVisible(prevVisible => !prevVisible)
     }
     return (
         <div className="worker">
-            <h2 className="worker-name">{worker.name}</h2>
-            <div className={ visible ? "visible" : "hidden"}>
+            <h2 className="worker-name">{state.name}</h2>
+            {/* <div className={ visible ? "visible" : "hidden"}>
                 <h2>Owned: <span>{owned}</span></h2>
                 <h2>Cost: <span>{cost * multiplier}</span>$</h2>
                 <h2>Production Rate: <span>{productionRate}</span>$</h2>
                 <h2>Upgrade Cost: <span>{productionRateUpgradeCost}</span></h2>
                 <button onClick={() => buyWorker()}>Buy <span>{multiplier}</span>?</button>
                 <button onClick={() => upgradeWorker()}>Upgrade?</button>
-            </div>
+            </div> */}
             <div>
                 <button 
                     tabIndex="0"
