bd41ec7ab6643a66e1d8418c5771c396fdb3d4d4
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-20 21:17:59 -0400

    custom hook for worker component simplification

--- src/components/Worker.js
+++ src/components/Worker.js
@@ -1,69 +1,21 @@
-import React, {useEffect, useReducer, useCallback} from 'react';
+import React from 'react';
 import useVisible from "../hooks/useVisible";
+import useWorker from "../hooks/useWorker";
 
-const ACTIONS = {
-    Upgrade: "UpgradeWorker",
-    Buy: "BuyWorker",
-    Mining: "MiningWorker",
-}
-function reducer(state, action) {
-    switch(action.type) {
-        case ACTIONS.Upgrade:
-            return {
-                ...state,
-                productionRate: state.productionRate + 1, 
-                productionRateCost: state.productionRateCost * 2, 
-            }
-        case ACTIONS.Buy:
-            return {
-                ...state,
-                owned: state.owned + action.payload,
-                cost: state.cost + action.payload, 
-            }
-        case ACTIONS.Mining:
-            // 
-            return {
-                ...state,
-            }
-        default: return state
-    }
-}
 
 export default function Worker({worker, money, setMoney, multiplier}) {
-    // set default state to worker and pass reducer function
-    const [state, dispatch] = useReducer(reducer, worker);
     const [workerVisibility, reveal] = useVisible();
-    const miningSpeed = 3000;
-
-
-    const BuyWorker = useCallback(() => {
-        const totalCost = state.cost * multiplier;
-        if (money < totalCost) return;
-        setMoney(prevMoney => prevMoney - totalCost);
-        dispatch({type: ACTIONS.Buy, payload: multiplier})
-    }, [state, multiplier, money])
-
-    const UpgradeWorker = useCallback(() => {
-        if (money < state.productionRateUpgradeCost) return;
-        setMoney(prevMoney => prevMoney - state.productionRateUpgradeCost);
-        dispatch({type: ACTIONS.Upgrade})
-    }, [state, money])
-
-    useEffect(() => {
-        setInterval(() => {
-            setMoney(prevMoney => prevMoney + (state.owned * state.productionRate))
-        }, miningSpeed)
-    }, [state, money])
+    const [workerState, BuyWorker, UpgradeWorker] = useWorker(worker, setMoney, money, multiplier);
     return (
         <div className="worker">
-            <h2 className="worker-name">{state.name}</h2>
+            <h2 className="worker-name">{workerState.name}</h2>
              <div
                 ref={workerVisibility} 
                 >
-                <h2>Owned: <span>{state.owned}</span></h2>
-                <h2>Cost: <span>{state.cost * multiplier}</span>$</h2>
-                <h2>Production Rate: <span>{state.productionRate}</span>$</h2>
-                <h2>Upgrade Cost: <span>{state.productionRateUpgradeCost}</span></h2>
+                <h2>Owned: <span>{workerState.owned}</span></h2>
+                <h2>Cost: <span>{workerState.cost * multiplier}</span>$</h2>
+                <h2>Production Rate: <span>{workerState.productionRate}</span>$</h2>
+                <h2>Upgrade Cost: <span>{workerState.productionRateUpgradeCost}</span></h2>
                 <button onClick={() => BuyWorker()}>Buy <span>{multiplier}</span>?</button>
                 <button onClick={() => UpgradeWorker()}>Upgrade?</button>
             </div>
---
+++ src/hooks/useWorker.js
@@ -0,0 +1,48 @@
+import { useReducer, useEffect, useCallback } from "react";
+
+const ACTIONS = {
+    Upgrade: "UpgradeWorker",
+    Buy: "BuyWorker",
+    Mining: "MiningWorker",
+}
+function reducer(state, action) {
+    switch(action.type) {
+        case ACTIONS.Upgrade:
+            return {
+                ...state,
+                productionRate: state.productionRate + 1, 
+                productionRateUpgradeCost: state.productionRateUpgradeCost * 2, 
+            }
+        case ACTIONS.Buy:
+            return {
+                ...state,
+                owned: state.owned + action.payload,
+                cost: state.cost + action.payload, 
+            }
+        default: return state;
+    }
+}
+export default function useWorker(worker, setMoney, money, multiplier) {
+    const [state, dispatch] = useReducer(reducer, worker);
+    const miningSpeed = 3000;
+
+    const BuyWorker = useCallback(() => {
+        const totalCost = state.cost * multiplier;
+        if (money < totalCost) return;
+        setMoney(prevMoney => prevMoney - totalCost);
+        dispatch({type: ACTIONS.Buy, payload: multiplier})
+    }, [state, multiplier, money])
+
+    const UpgradeWorker = useCallback(() => {
+        if (money < state.productionRateUpgradeCost) return;
+        setMoney(prevMoney => prevMoney - state.productionRateUpgradeCost);
+        dispatch({type: ACTIONS.Upgrade})
+    }, [state, money])
+
+    useEffect(() => {
+        setInterval(() => {
+            setMoney(prevMoney => prevMoney + (state.owned * state.productionRate))
+        }, miningSpeed)
+    }, [state])
+    return [state, BuyWorker, UpgradeWorker];
+}
