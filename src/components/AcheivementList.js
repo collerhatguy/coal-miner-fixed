442e57b5468eb3c08d5b7b13c42db9a186c20da9
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-05-19 10:37:33 -0400

    gonna come back later

--- src/components/AcheivementList.js
+++ src/components/AcheivementList.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function AcheivementList() {
     return (
         <div id="acheivement-list">
-            
+            <div>
+                Get one of every Worker
+            </div>
         </div>
     )
 }
