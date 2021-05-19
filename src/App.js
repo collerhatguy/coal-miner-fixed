b087e3f30e1839d0ecc09b1e1f4102c3551f3509
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-05-19 10:46:50 -0400

    deleted old comment

--- src/App.js
+++ src/App.js
@@ -5,16 +5,13 @@ import "./css/App.css";
 import MultiplierList from "./components/MultiplierList";
 
 function App() {
-  // set everything back to default values based on local save
   const [money, setMoney] = useState(10
     // JSON.parse(localStorage.getItem("CoalMiner.money")) || 10
   );
   const [multiplier, setMultiplier] = useState(1);
-  // for saving money
   useEffect(() => {
     localStorage.setItem("GoldMiner.money", JSON.stringify(money))
   }, [money])
-
   return (
     <>
       <h2>Current Money: {money}</h2>
