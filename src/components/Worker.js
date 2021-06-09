f61e55a869b4424420d1aa78772614c648e533b4
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-06-08 21:17:18 -0400

    fixed scss and added reset with simplified styles

--- src/components/Worker.js
+++ src/components/Worker.js
@@ -23,7 +23,7 @@ export default function Worker({worker, money, setMoney, multiplier}) {
                 <h4>Owned: <span>{workerState.owned}</span></h4>
                 <h4>Cost: <span>{workerState.cost * multiplier}</span>$</h4>
                 <h4>Production Rate: <span>{workerState.productionRate}</span>$</h4>
-                <h4>Upgrade Cost: <span>{workerState.productionRateUpgradeCost}</span></h4>
+                <h4>Upgrade Cost: <span>{workerState.productionRateUpgradeCost}</span>$</h4>
                 <h4>Level: <span>{workerState.level}</span></h4>
                 <button onClick={() => BuyWorker()}>Buy <span>{multiplier}</span>?</button>
                 <button onClick={() => UpgradeWorker()}>Upgrade?</button>
