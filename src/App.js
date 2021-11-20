b0f818849d5f2e723b731398f05cc693e0b956e8
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-11-20 16:41:39 -0600

    fixed multiplier bug

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import WorkerList from "./components/WorkerList";
 import MultiplierList from "./components/MultiplierList";
 
 function App() {
-  const [money, setMoney] = useState(10);
+  const [money, setMoney] = useState(1000);
   const [multiplier, setMultiplier] = useState(1);
   const [showMultipliers, toggleShowMultipliers] = useToggle(false);
-  
+
   useEffect(() => {
+    if (showMultipliers) return;
     if (money >= 1000) toggleShowMultipliers(true);
   }, [money])
 
